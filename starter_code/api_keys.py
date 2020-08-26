6bbf9223a26c62b7c6d4fa9679d3aab63654ee32
SakiSugio <67557479+SakiSugio@users.noreply.github.com>
2020-08-25 20:50:43 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b66506d86c28e3321e7e88b49d4983d5"
+weather_api_key = "Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyAyoYOK3elADQJyScjYk7u2ukQCOVeg4HU"
+g_key = "Your Key Here"
